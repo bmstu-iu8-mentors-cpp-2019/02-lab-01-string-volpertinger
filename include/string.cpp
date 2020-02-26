f1e72a984598e3e044ffa4fe05d432bbf4c01174
Nikolay <merzlovnik@mail.ru>
2020-02-26 23:24:20 +0300

    -_-

---
+++ include/string.cpp
@@ -0,0 +1,2 @@
+// Copyright 2020 Merzlov Nikolay merzlovnik@mail.ru
+#unclude "strng.hpp"
--- include/string.hpp
+++ include/string.hpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Merzlov Nikolay merzlovnik@mail.ru
 
 #ifndef INCLUDE_STRING_HPP_
 #define INCLUDE_STRING_HPP_
